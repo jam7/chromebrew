63a4342f27a0238634d5644146a2b4e02b8f6258
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-08-27 15:22:30 +0900

    Add cairo and harfbuzz precompiled binaries

--- packages/cairo.rb
+++ packages/cairo.rb
@@ -8,8 +8,16 @@ class Cairo < Package
   source_sha256 '7e87878658f2c9951a14fc64114d4958c0e65ac47530b8ac3078b2ce41b66a09'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cairo-1.14.10-2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cairo-1.14.10-2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/cairo-1.14.10-2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cairo-1.14.10-2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '702c142917c4c56dad6033b1cc06c8437e8caf3d0bdac9412d5fe17acee94e86',
+     armv7l: '702c142917c4c56dad6033b1cc06c8437e8caf3d0bdac9412d5fe17acee94e86',
+       i686: 'a7e5aa52792bbf3a3583ddeee98f4c6cafd9ade05f1824e2f71c214f123176fb',
+     x86_64: '452b001317f7ddbecb964f1e053b8d0d1e49855c014fdbd1cc7a201311437f34',
   })
 
   depends_on 'libpng'
--- packages/harfbuzz.rb
+++ packages/harfbuzz.rb
@@ -8,8 +8,16 @@ class Harfbuzz < Package
   source_sha256 'ccec4930ff0bb2d0c40aee203075447954b64a8c2695202413cc5e428c907131'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/harfbuzz-1.4.8-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/harfbuzz-1.4.8-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/harfbuzz-1.4.8-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/harfbuzz-1.4.8-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '57b13a68bb651f36ab6918f32dc5656bf6efe4855b5a7a0132f683fea12eb8fa',
+     armv7l: '57b13a68bb651f36ab6918f32dc5656bf6efe4855b5a7a0132f683fea12eb8fa',
+       i686: 'e77a016c6e51db694fba02b4f8f42667bbdebcce4eb0f5a0612bfa9940ffd8a9',
+     x86_64: '37eb4a8b5114e1e2e5a4334f91be6fcaffa2f4a5e42a3df30cecf542c6a889b4',
   })
 
   depends_on 'glib'
